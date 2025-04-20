a93536feec5fcc7bc0b5b3a5155cdddb05771b39
kenhollingsworth422 <kenhollingsworth422@gmail.com>
2025-04-20 18:02:25 -0400

    make function to pass all the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let newStr = str;
+    if (num == 0){
+        return ""
+    }
+    else if (num < 0){
+        return "ERROR"
+    }
+    for (i=0;i<=num-2;i++){
+        newStr = newStr.concat(str);
+    }
+    return newStr
+    
+
 
 };
 
